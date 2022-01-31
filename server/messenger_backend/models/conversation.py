c87f225ab219785bddd3bb7c273b5dce12111fce
Gregory Fleming <flemdog13@hotmail.com>
2022-01-30 18:40:38 -0800

    Backend Models Multiple Participants Update
    Updated the backend Conversation model to allow multiple users to participate in a single conversation.

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,29 +1,25 @@
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from . import utils
 from .user import User
 
-
 class Conversation(utils.CustomModel):
-
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    )
+    participants = models.ManyToManyField(User)
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
     user1QtyUnread = models.IntegerField(default=0)
     user2QtyUnread = models.IntegerField(default=0)
+    
     # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
+    def find_conversation(participants):
         # return conversation or None if it doesn't exist
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
-        except Conversation.DoesNotExist:
+            #Filter the conversation so that only the conversation containing all the participants is returned.
+            conversation = Conversation.objects.filter(participants__in=participants).annotate(num_participants=Count('participants')).filter(num_participants=len(participants))
+            if conversation:
+                return conversation
+            else:
+                return None
+        except:
             return None
